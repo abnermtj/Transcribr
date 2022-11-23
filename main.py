af514a9ffdca894016e569605f1c2c5582e70230
abner <abnermtj@live.com.sg>
2022-11-23 15:45:22 +0800

    Add dedicated input output folder

--- main.py
+++ main.py
@@ -42,10 +42,10 @@ def to_srt(transcript: Iterator[dict]):
     return out
 
 def scribe(audio_path):
+    print(audio_path)
     result = model.transcribe(audio_path)
-
-    with open(os.path.join(audio_path + ".srt"), "w", encoding="utf-8") as srt:
-        return to_srt(result["segments"])
+    print(result)
+    return to_srt(result["segments"])
 
 def _max_width_():
     max_width_str = f"max-width: 1600px;"
@@ -87,7 +87,7 @@ def transcribe_process():
     uploader_file_list = st.file_uploader("Upload Audio and Video files to transcribe", type=['mkv', 'mp4','avi','wav', 'mp3'], accept_multiple_files=True)
     st.info(
             f"""
-            👆 Upload Multiple Audio or Video files. 
+            👆 Upload Multiple Audio or Video files 
             """
             )
 
@@ -95,34 +95,40 @@ def transcribe_process():
         text_value = ""
         if uploaded_file is not None:
             file_name = uploaded_file.name
+            input_file_path = 'input/' + uploaded_file.name
+            pre, ext = os.path.splitext(file_name)
+            output_file_path = './output/' + pre + ".mp3"
 
-            with open(file_name, mode='wb') as save:
+            with open(input_file_path, mode='wb') as save:
                 save.write(uploaded_file.read()) # save video to disk
-
-            audio = AudioSegment.from_file(file_name)
-            audio.export("./output/file.mp3", format="mp3")
+                audio = AudioSegment.from_file(input_file_path)
+                audio.export(output_file_path, format="mp3")
 
             file_size = get_file_size(uploaded_file)
             st.caption("Working On " + file_name + "(" + str(file_size) + " MB)")
 
-            text_value =  scribe(file_name)
+            text_value =  scribe(output_file_path)
             if text_value:
                 col1, col2 = st.columns(2)
                 # Print the output to your Streamlit app
                 col1.success(file_name + " is done")
 
-                pre, ext = os.path.splitext(file_name)
-
                 col2.download_button(
                 "Download .srt",
                 text_value,
-                file_name= pre+'.srt',
+                file_name= pre + '.srt',
                 mime=None,
                 key=None,
                 help=None,
                 on_click=None,
                 args=None,
                 kwargs=None,
+                )
+            else:
+                st.error(
+                f"""
+                No subtitles found.
+                """
                 )
 
     expander = st.expander("History")
